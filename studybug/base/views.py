568f9dc387ef815e6a6d8a3a35e7a5d76b86d2dc
Ashish Chaudhary <ashish080303@gmail.com>
2023-07-08 13:32:32 +0530

    form created and saved

--- studybug/base/views.py
+++ studybug/base/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -21,3 +22,14 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {"room": room}
     return render(request, "base/room.html", context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
