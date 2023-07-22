65ccf234e5b2b11a194e3a5bf2c9fa380f1635df
Ashish Chaudhary <ashish080303@gmail.com>
2023-07-22 13:10:35 +0530

    rest-framework is progressed

--- studybug/base/api/views.py
+++ studybug/base/api/views.py
@@ -1,10 +1,28 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializer
 
+@api_view(['GET'])
 def getRoutes(request):
   routes=[
     'GET /api',
     'GET /api/rooms',
-    'GET /api/rooms/:id',
+    'GET /api/room/:id',
     
   ]
-  return JsonResponse(routes,safe=False)
+  # rest automatically serializes the lists gives, you don't need to pass safe=False method here like in JsonResponse
+  return Response(routes)
+
+@api_view(['GET'])
+def getRooms(request):
+  rooms = Room.objects.all()
+  serializer = RoomSerializer(rooms,many=True)
+  #this alone passes the objects and not the list. So, using serializers.py, we serialize every object into the json format
+  return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request,pk):
+  room = Room.objects.get(id=pk)
+  serializer = RoomSerializer(room,many=False)
+  return Response(serializer.data)
